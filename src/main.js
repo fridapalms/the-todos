508c3030569b7c8564973c0bb9d34b93ba17496f
Frida Palm <fridapalm@MacBook-Pro-som-tillhor-Frida.local>
2025-10-24 11:35:14 +0200

    bootstrap setup

--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import "./style.scss";
+
 
 const app = document.getElementById("app");
 const testDiv = document.createElement("div");
